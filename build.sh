3c0cef7ace82188a8c62df9c25c32d1ffdd6156f
Ashot Vardanian <ashvardanian@gmail.com>
2021-01-22 16:37:49 +0400

    All containers now derive from each other and are VSCode-compatiable

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 docker build --tag unum/devcontainers:vscode-cpp20 vscode-cpp20
 docker build --tag unum/devcontainers:vscode-cpp20-cuda11 vscode-cpp20-cuda11
-docker build --tag unum/devcontainers:jupyter-graphml jupyter-graphml
+docker build --tag unum/devcontainers:vscode-graphml vscode-graphml
+
+docker image push --all-tags unum/devcontainers
