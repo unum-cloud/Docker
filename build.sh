051f35f2fbd70b1f6c8c8667fdb97292d5040569
Ashot Vardanian <ashvardanian@gmail.com>
2021-01-24 18:53:49 +0400

    Minor

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 docker build --tag unum/devcontainers:vscode-cpp20 vscode-cpp20
 docker build --tag unum/devcontainers:vscode-cpp20-cuda11 vscode-cpp20-cuda11
 docker build --tag unum/devcontainers:jupyter-graphml jupyter-graphml
+
+# https://docs.docker.com/engine/reference/commandline/push/#push-all-tags-of-an-image
+docker image push --all-tags unum/devcontainers
+
+docker push ghcr.io/unumam/containers:vscode-cpp20
