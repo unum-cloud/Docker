bb914b811e1245de6ac6260bc01a98dcfc08cfbf
Ashot Vardanian <ashvardanian@gmail.com>
2021-01-24 19:30:50 +0400

    Moving from Docker Hub to GitHub Package registry and changing the naming scheme.

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
-docker build --tag unum/devcontainers:vscode-cpp20 vscode-cpp20
-docker build --tag unum/devcontainers:vscode-cpp20-cuda11 vscode-cpp20-cuda11
-docker build --tag unum/devcontainers:jupyter-graphml jupyter-graphml
+docker build --tag unumam/dev:vscode-cpp20 vscode-cpp20
+docker build --tag unumam/dev:vscode-cpp20-cuda11 vscode-cpp20-cuda11
+docker build --tag unumam/dev:vscode-graphml vscode-graphml
+
+docker tag unumam/dev:vscode-cpp20 docker.pkg.github.com/unumam/dockercontainers/dev:vscode-cpp20
+docker tag unumam/dev:vscode-cpp20-cuda11 docker.pkg.github.com/unumam/dockercontainers/dev:vscode-cpp20-cuda11
+docker tag unumam/dev:vscode-graphml docker.pkg.github.com/unumam/dockercontainers/dev:vscode-graphml
 
 # https://docs.docker.com/engine/reference/commandline/push/#push-all-tags-of-an-image
-docker image push --all-tags unum/devcontainers
 
-docker push ghcr.io/unumam/containers:vscode-cpp20
+# Generate a GitHub access token in
+#       "Settings" -> "Developer Settings" -> "Personal Access Token"
+# docker login ghcr.io -u ashvardanian
+# docker login https://docker.pkg.github.com -u ashvardanian
+docker image push --all-tags docker.pkg.github.com/unumam/dockercontainers/dev
