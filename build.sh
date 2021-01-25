bfd2b61afef6ebc93619b5ab828aa8682081db56
Ashot Vardanian <ashvardanian@gmail.com>
2021-01-25 13:54:24 +0400

    Migrated to GHCR, because the older Github Package registry requires authentication to access even public containers

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@
 #       "Settings" -> "Developer Settings" -> "Personal Access Token"
 # Then authenticate by passing that token as password:
 #       docker login ghcr.io -u ashvardanian
-#       docker login https://docker.pkg.github.com -u ashvardanian
+#       docker login https://ghcr.io -u ashvardanian
 
 docker build --tag unumam/vscode-cpp20 vscode-cpp20 &&
-    docker tag unumam/vscode-cpp20 docker.pkg.github.com/unumam/docker/vscode-cpp20 &&
-    docker image push --all-tags docker.pkg.github.com/unumam/docker/vscode-cpp20
+    docker tag unumam/vscode-cpp20 ghcr.io/unumam/vscode-cpp20 &&
+    docker image push --all-tags ghcr.io/unumam/vscode-cpp20
 
 docker build --tag unumam/vscode-cpp20-cuda11 vscode-cpp20-cuda11 &&
-    docker tag unumam/vscode-cpp20-cuda11 docker.pkg.github.com/unumam/docker/vscode-cpp20-cuda11 &&
-    docker image push --all-tags docker.pkg.github.com/unumam/docker/vscode-cpp20-cuda11
+    docker tag unumam/vscode-cpp20-cuda11 ghcr.io/unumam/vscode-cpp20-cuda11 &&
+    docker image push --all-tags ghcr.io/unumam/vscode-cpp20-cuda11
 
 docker build --tag unumam/vscode-graphml vscode-graphml &&
-    docker tag unumam/vscode-graphml docker.pkg.github.com/unumam/docker/vscode-graphml &&
-    docker image push --all-tags docker.pkg.github.com/unumam/docker/vscode-graphml
+    docker tag unumam/vscode-graphml ghcr.io/unumam/vscode-graphml &&
+    docker image push --all-tags ghcr.io/unumam/vscode-graphml
